3184add4b9b960397a3d656df24bdb2d087a5d60
Will Anderson <will@itsananderson.com>
2013-03-24 16:05:17 -0700

    Make newline removal more robust, to handle Twitter's pushstate behavior

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-var linebreakObject = document.querySelector('.tweet-display-linebreaks');
-linebreakObject.className = linebreakObject.className.replace('tweet-display-linebreaks', '');
+(function() {
+
+	var debugging = false;
+	var linebreakClassname = 'tweet-display-linebreaks';
+	var linebreakObject = document.querySelector('.' + linebreakClassname);
+
+	var debug = function(value) {
+		if ( debugging ) {
+			console.log(value);
+		}
+	}
+	
+	var rmClassname = function(el, className) {
+		if ( -1 !== el.className.indexOf(className) ) {
+			el.className = el.className.replace(new RegExp('\s*' + className + '\s*'), '');
+			debug('Removed class "' + className + '"');
+		} else {
+			debug('No class "' + className + '". Nothing to do');
+		}
+	};
+	
+	rmClassname(linebreakObject, linebreakClassname);
+
+	var observer = new WebKitMutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			if (mutation.attributeName == 'class') {
+				rmClassname(linebreakObject, linebreakClassname);
+			}
+		});
+	});
+	observer.observe(linebreakObject, { attributes: true });
+})();
